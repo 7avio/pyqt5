9dfa3e83d20f3df0a46d0b45491a3ba23d753dc5
Favio Varela <7avio.contact@gmail.com>
2023-01-26 22:56:18 -0600

    deleting failed files

--- pass2pdf.py
+++ pass2pdf.py
@@ -1,11 +1,13 @@
-from PyPDF2 import PdfFileWriter, PdfFileReader
+from PyPDF2 import PdfWriter, PdfReader
 import getpass
+import sys
 
-pdfwriter = PdfFileWriter()
-pdf = PdfFileReader("example.pdf")
-for page_num in range(pdf.numPages):
-	pdfwriter.addPage(pdf.getPage(page_num))
-passw = getpass.getpass(prompt = 'Enter your desired passwprd: ')
+pdfwriter = PdfWriter()
+pdf = PdfReader("example.pdf")
+for page_num in range(len(pdf.pages)):
+	pdfwriter.add_page(pdf.pages[page_num])
+# passw = getpass.getpass(prompt = 'Enter your desired passwprd: ')
+passw = getpass.win_getpass(prompt='Password: ', stream=None)
 pdfwriter.encrypt(passw)
 with open("new.pdf", "wb") as f:
 	pdfwriter.write(f)
