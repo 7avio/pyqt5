adde9d86f3b4a128abd1ecbea79b16ba49f3a992
Favio Varela <7avio.contact@gmail.com>
2023-01-28 13:35:45 -0600

    Replace with a function

---
+++ main.py
@@ -0,0 +1,4 @@
+import pass2pdf
+
+if __name__ == '__main__':
+    pass2pdf.encrypt_document()
--- pass2pdf.py
+++ pass2pdf.py
@@ -2,12 +2,15 @@ from PyPDF2 import PdfWriter, PdfReader
 import getpass
 import sys
 
-pdfwriter = PdfWriter()
-pdf = PdfReader("example.pdf")
-for page_num in range(len(pdf.pages)):
-	pdfwriter.add_page(pdf.pages[page_num])
-# passw = getpass.getpass(prompt = 'Enter your desired passwprd: ')
-passw = getpass.win_getpass(prompt='Password: ', stream=None)
-pdfwriter.encrypt(passw)
-with open("new.pdf", "wb") as f:
-	pdfwriter.write(f)
+def encrypt_document():
+	pdf_file = str(input('insert the name of your pdf file: '))
+	pdf_pass = str(input('insert the name of your document with password'))
+	pdfwriter = PdfWriter()
+	pdf = PdfReader(pdf_file)
+	for page_num in range(len(pdf.pages)):
+		pdfwriter.add_page(pdf.pages[page_num])
+	# passw = getpass.getpass(prompt = 'Enter your desired passwprd: ')
+	passw = getpass.win_getpass(prompt='Password: ', stream=None)
+	pdfwriter.encrypt(passw)
+	with open(pdf_pass, 	"wb") as f:
+		pdfwriter.write(f)
